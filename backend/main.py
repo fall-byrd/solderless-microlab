b7f398d66d9ac551c7e69ddf33de6e806ad28177
Fall Byrd <fall.byrd@protonmail.com>
2024-11-04 21:24:31 -0800

    Cleaned up debugging in main.py and microlab/core.py

--- backend/main.py
+++ backend/main.py
@@ -5,23 +5,22 @@ Starts the flask application on the configured port (default 8081)
 Look in api.routes for the actual api code
 """
 
-import signal
+import sys
 import time
+import signal
+import logging
+import logging.handlers as logging_handlers
 
-import requests
-
-from multiprocessing import Process, Queue, Value, set_start_method
-from microlab.core import MicrolabHardwareManager
-from api.core import run_flask
-import config
 import multiprocessing_logging
 
-import logging
-import logging.handlers as handlers
-from util.logFormatter import MultiLineFormatter
-import sys
+import config
 
-from hardware.core import MicroLabHardware
+from multiprocessing import Process, Queue, set_start_method
+
+from api.core import run_flask
+from microlab.core import MicrolabHardwareManager
+from util.logFormatter import MultiLineFormatter
+# from hardware.core import MicroLabHardware
 
 
 LOGGER = logging.getLogger(__name__)
@@ -31,7 +30,7 @@ def setupLogging():
     logHandlers = []
     formatter = MultiLineFormatter(fmt="%(asctime)s [%(levelname)s]: %(message)s")
 
-    fileLogger = handlers.RotatingFileHandler(
+    fileLogger = logging_handlers.RotatingFileHandler(
         "{0}/microlab.log".format(config.microlabConfig.logDirectory),
         maxBytes=config.microlabConfig.logFileMaxBytes,
         backupCount=config.microlabConfig.logFileBackupCount,
@@ -50,64 +49,30 @@ def setupLogging():
 class BackendManager:
 
     def __init__(self):
-        # self._microlab_manager_should_run = Value('i', 1)
         self._q1 = Queue()
         self._q2 = Queue()
 
         self._processes = []
         self._queues = []
 
-    # def _shutdown_flask(self):
-    #     shutdown_url = f'http://localhost:{config.microlabConfig.apiPort}/shutdown'
-    #     print(f'_shutdown_flask: shutdown_url: {shutdown_url}')
+    def _are_processes_alive(self) -> bool:
+        return any([process.is_alive() for process in self._processes])
 
-    #     shutdown_request_sent = False
-    #     for i in range(5):
-    #         try:
-    #             response = requests.put(shutdown_url, timeout=1)
-    #             print(f'_shutdown_flask: response code: {response.status_code}')
-    #             shutdown_request_sent = True
-    #             break
-    #         except Exception as e:
-    #             print(f'Encountered exception {e} when attempting to shutdown Flask server')
+    def _cleanup_queues(self):
+        LOGGER.debug('Cleaning up queues')
 
-    #     if not shutdown_request_sent:
-    #         # We tried being nice, let's be a bit more forceful
-    #         sys.stdout.write('Attempting to forcefully terminate _flaskProcess\n')
-    #         sys.stdout.flush()
-    #         self._flaskProcess.terminate()
-    #         sys.stdout.write('Completed attempt to forcefully terminate _flaskProcess\n')
-    #         sys.stdout.flush()
+        self._q1.close()
+        self._q2.close()
 
-        # try:
-        # self._flaskProcess.terminate()
-        # except ValueError as e:
-        #     print(e)
-        #     # When you terminate the process this way it will kill it however it will
-        #     # raise a secondary exception when killing the process, claiming there is no process to kill
-        #     # even though there certainly is. I suspect this may be Flasks behavior in how it handles signals
-        #     pass
+        self._q1.join_thread()
+        self._q2.join_thread()
 
-        # except AttributeError as e:
-        #     print(e)
-        #     pass
+        LOGGER.debug('Completed cleanup up queues')
 
-    def _handle_exit_signals(self, signum, frame):
-        print('in _handle_exit_signals')
-        # self._microlab_manager_should_run.value = 0
-        # self._shutdown_flask()
-        self._cleanup_everything()
+    def _cleanup_processes(self):
+        LOGGER.debug('Cleaning up processes')
 
-        # print('in _handle_exit_signals closing queue 1')
-        # self._q1.close()
-        # print('in _handle_exit_signals closing queue 2')
-        # self._q2.close()
-        # print('in _handle_exit_signals queues closed')
-
-    def _cleanup_everything(self):
-        print('In _cleanup_everything')
-
-        while any([process.is_alive() for process in self._processes]):
+        while self._are_processes_alive():
             for proc in self._processes:
                 try:
                     self._q1.get_nowait()
@@ -120,88 +85,52 @@ class BackendManager:
                     pass
 
                 if proc.is_alive():
-                    print(f'Attempting to join proc: {proc.pid}')
+                    LOGGER.debug(f'Attempting to join proc: {proc.pid}')
                     proc.join(timeout=1)
 
-        print('MAIN before q1 close')
-        self._q1.close()
+        LOGGER.debug('Completed cleaning up processes')
 
-        print('MAIN before q2 close')
-        self._q2.close()
+    def _cleanup_everything(self):
+        self._cleanup_processes()
+        self._cleanup_queues()
 
-        # print('MAIN before q1 close')
-        # self._q1.close()
-        print('MAIN before q1 join')
-        self._q1.join_thread()
-        # print('MAIN before q2 close')
-        # self._q2.close()
-        print('MAIN before q2 join')
-        self._q2.join_thread()
+    def _handle_exit_signals(self, signum, frame):
+        LOGGER.debug('Beginning to handle exit signals in BackendManager')
+        self._cleanup_everything()
+        LOGGER.debug('Completed handling exit signals in BackendManager')
 
-    def _are_processes_alive(self) -> bool:
-        return any([process.is_alive() for process in self._processes])
-        
+    def _start_microlab(self):
+        self._microlab_manager_process = MicrolabHardwareManager(
+            self._q1, self._q2
+        )
+        self._processes.append(self._microlab_manager_process)
+
+        LOGGER.debug('Starting the microlab process')
+        self._microlab_manager_process.start()
+        LOGGER.debug(f'microlab process pid: {self._microlab_manager_process.pid}')
+
+    def _start_server(self):
+        self._flaskProcess = Process(target=run_flask, args=(self._q2, self._q1), name="flask", daemon=True)
+        self._processes.append(self._flaskProcess)
+        LOGGER.debug('Starting the server process')
+        self._flaskProcess.start()
+        print(f'server process pid: {self._flaskProcess.pid}')
+
     def run(self):
         config.initialSetup()
 
         LOGGER.info("### STARTING MAIN MICROLAB SERVICE ###")
 
-        print('MAIN before')
-        # We're setting up a shared memory value here so that if the main process recieves a signal to terminate
-        # we can update the value to indicate to the process that it needs to terminate
-        
-        # TODO Move creation of this into the the HWManager process
-        self._microlab_hardware = MicroLabHardware.get_microlab_hardware_controller()
-        # self._microlab_manager = MicrolabHardwareManager(
-        #     self._microlab_hardware, q1, q2, self._microlab_manager_should_run
-        # )
-        self._microlab_manager_process = MicrolabHardwareManager(
-            self._microlab_hardware, self._q1, self._q2
-        )
-        print('MAIN before lab start')
-        self._processes.append(self._microlab_manager_process)
-        # self._microlab_manager_process = Process(target=self._microlab_manager.run, name="microlab")
-        self._microlab_manager_process.start()
-        print(f'MAIN self._microlab_manager_process pid: {self._microlab_manager_process.pid}')
+        self._start_microlab()
+        self._start_server()
 
-        self._flaskProcess = Process(target=run_flask, args=(self._q2, self._q1), name="flask", daemon=True)
-        self._processes.append(self._flaskProcess)
-        print('MAIN before flask start')
-        self._flaskProcess.start()
-
-        print(f'MAIN self._flaskProcess pid: {self._flaskProcess.pid}')
         signal.signal(signal.SIGINT, self._handle_exit_signals)
         signal.signal(signal.SIGTERM, self._handle_exit_signals)
 
         while self._are_processes_alive():
             time.sleep(0.1)
 
-        # print('MAIN before flask join')
-        # self._flaskProcess.join()
-
-        # print('MAIN before lab join')
-        # try:
-        #     # self._microlab_manager_process.join()
-        # except Exception as e:
-        #     # We re-raise any exceptions in execution and if we see them we need to shut down flask
-        #     print(f'Hit lab exception: {e}')
-        #     self._shutdown_flask()
-        #     # raise
-        #     import sys
-        #     sys.exit(1)
-
-        # self._cleanup_everything()
-
-        # print('MAIN before q1 close')
-        # self._q1.close()
-        # print('MAIN before q1 join')
-        # self._q1.join_thread()
-        # print('MAIN before q2 close')
-        # self._q2.close()
-        # print('MAIN before q2 join')
-        # self._q2.join_thread()
-        print('MAIN run done')
-        # sys.exit(0)
+        LOGGER.debug("### ENDING MICROLAB SERVICE EXECUTION ###")
 
 
 def main():
--- backend/microlab/core.py
+++ backend/microlab/core.py
@@ -5,14 +5,12 @@ import logging
 import time
 import traceback
 import signal
-import os
 import sys
 
 import hardware.devicelist
 import recipes.core
 import recipes.state
 
-# from threading import Thread, Event, Lock
 from multiprocessing import Queue, Process
 from typing import Optional
 from queue import Empty
@@ -21,25 +19,19 @@ from config import microlabConfig as config
 from hardware.core import MicroLabHardware
 
 
-# HALT = Event()
-# MUTEX = Lock()
-
 LOGGER = logging.getLogger(__name__)
 
 
 class MicrolabHardwareManager(Process):
 
     def __init__(
-        self, microlab_hardware: MicroLabHardware, in_queue: Queue, out_queue: Queue,
+        self, in_queue: Queue, out_queue: Queue,
         *args, **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self._microlab_hardware: MicroLabHardware = microlab_hardware
+        self._microlab_hardware: MicroLabHardware = MicroLabHardware.get_microlab_hardware_controller()
         self._in_queue = in_queue
         self._out_queue = out_queue
-        # should_run is actually a multiprocessing.Value that is an int, if the main thread recieves a signal
-        # to exit it will update this shared value. It is typed as an int as opposed to multiprocessing.Value
-        # because when typed as a 'Value' (by mypy at least) it shows as an invalid type
         self._should_run = True
 
         self._command_dict = {
@@ -58,7 +50,7 @@ class MicrolabHardwareManager(Process):
         signal.signal(signal.SIGTERM, self._shutdown)
 
     def _shutdown(self, signum, frame):
-        LOGGER.info('Begining microlab shutdown process.')
+        LOGGER.debug('Begining microlab shutdown process.')
         self._should_run = False
 
     def _close_out_queue(self):
@@ -70,28 +62,15 @@ class MicrolabHardwareManager(Process):
                 break
 
     def _cleanup(self):
-        LOGGER.info("")
         LOGGER.info("Shutting down microlab.")
         self._microlab_hardware.turnOffEverything()
         LOGGER.info("Shutdown completed.")
 
-        sys.stdout.write('Begining purge of MicrolabHardwareManager _out_queue\n')
-        sys.stdout.flush()
+        LOGGER.debug('Begining purge of MicrolabHardwareManager out queue')
         self._close_out_queue()
-        sys.stdout.write('Completed purge of MicrolabHardwareManager _out_queue\n')
-        sys.stdout.flush()
-
-        # sys.stdout.write('Beginning logging shutdown in MicrolabHardwareManager\n')
-        # sys.stdout.flush()
-        # logging.shutdown()
-        # sys.stdout.write('Completed logging shutdown in MicrolabHardwareManager\n')
-        # sys.stdout.flush()
+        LOGGER.debug('Completed purge of MicrolabHardwareManager out queue')
 
         sys.exit(0)
-        # os._exit(os.EX_OK)
-        # import sys
-        # sys.exit()
-        # self._out_queue.close()
 
     def _run_command(self, command_string: str, command_args: Optional[str]) -> Optional[str]:
         result = None
@@ -107,40 +86,14 @@ class MicrolabHardwareManager(Process):
     def _update_queue_data(self):
         if not self._in_queue.empty():
             data = self._in_queue.get(timeout=5)  # Receive data
-            # status just fetches data and so doesn't need a lock, everything
-            # else is a mutation and needs a lock to prevent conflicts with
-            # the other thread
             result = self._run_command(data["command"], data.get("args", None))
             if result is not None:
                 self._out_queue.put(result, timeout=5)  # Send data back
-
-        # while True:
-        #     time.sleep(0.01)
-        #     if not self._in_queue.empty():
-        #         data = self._in_queue.get() # Receive data
-        #         # status just fetches data and so doesn't need a lock, everything
-        #         # else is a mutation and needs a lock to prevent conflicts with
-        #         # the other thread
-        #         if data["command"] == "status":
-        #             result = self._command_dict[data["command"]](data["args"])
-        #         else:
-        #             with self._lock:
-        #                 if data["args"]:
-        #                     result = self._command_dict[data["command"]](data["args"])
-        #                 else:
-        #                     result = self._command_dict[data["command"]]
-
-        #         if result is not None:
-        #             self._out_queue.put(result)  # Send data back
 
     def _update_microlab(self):
         if recipes.state.currentRecipe:
             recipes.state.currentRecipe.tickTasks()
             recipes.state.currentRecipe.checkStepCompletion()
-            
-        # if HALT.is_set():
-        #     self._microlab_hardware.turnOffEverything()
-        #     break
 
     def _reload_hardware(self):
         LOGGER.info("Reloading microlab device configuration")
@@ -149,152 +102,17 @@ class MicrolabHardwareManager(Process):
         return self._microlab_hardware.loadHardware(deviceDefinitions)
 
     def run(self):
-        # We setup signal handlers here as this is what is the target of (multiprocessing) Process.run. 
-        # If signal handlers are setup before existing in their own process they will be setup for the
-        # base context *and* be inherited by the new Process being started
         self._setup_signal_handlers()
 
-        # Any non-zero value will evalutate to True, self._should_run can be changed by either
-        # the spawning process recieving an exit signal and updating the value (it is a multiprocess.Value),
-        # or this process recieving an exit signal directly. This way we should make a best effort to shutdown
-        # the associated hardware
         while self._should_run:
-            # print('run loop pre sleep')
             time.sleep(0.01)
             try:
-                # print('run loop pre _update_queue_data')
                 self._update_queue_data()
-                # print('run loop pre _update_microlab')
                 self._update_microlab()
-                # print('run loop post _update_microlab')
             except Exception as e:
                 self._execution_exception = e
                 LOGGER.error(f'While running microlab hardware encountered exception: {e}. Shutting down microlab.')
                 LOGGER.debug(traceback.print_exc())
                 break
 
-        # print('run loop pre _cleanup')
         self._cleanup()
-        # print('run loop post _cleanup')
-
-    # def join(self, *args, **kwargs):
-    #     super().join(*args, **kwargs)
-
-    #     if self._execution_exception:
-    #         raise self._execution_exception
-
-
-# def startMicrolabProcess(in_queue, out_queue):
-    """
-    Starts up the microlab process
-
-    :param in_queue:
-        The queue the microlab will listen for commands on
-
-    :param out_queue:
-        The queue responses will be sent to, when applicable.
-
-    All commands sent over the queue have the following format:
-
-    dict
-        command
-            String with the following values: 
-                "start"
-                "stop"
-                "status"
-                "selectOption"
-        args
-            structure is determined by command type, see following section
-
-
-    Supported command and return values:
-        "start":
-            Begins the execution of a recipe
-            args structure
-                string - name of the recipe to start execution
-            returns
-                (True, '') on success.
-                (False, message) on failure.
-            reference "start" in /recipes/__init__.py for more info
-        "stop":
-            Stops the execution of a recipe
-            args structure
-                None
-            returns
-                None
-            reference "stop" in /recipes/__init__.py for more info
-        "status":
-            Gets the status of the microlab
-            args structure
-                None
-            returns
-                large object, see reference            
-            reference "status" in /recipes/__init__.py for more info
-        "selectOption":
-            Selects an option when a recipe requires user input
-            args structure
-                string - option the user selected
-            returns
-                (True, '') on success.
-                (False, message) on failure.
-            reference "selectOption" in /recipes/__init__.py for more info
-    """
-    # microlabHardware = MicroLabHardware.get_microlab_hardware_controller()
-
-    # def runMicrolab():
-    #     while True:
-    #         time.sleep(0.01)
-    #         MUTEX.acquire()
-    #         if recipes.state.currentRecipe:
-    #             recipes.state.currentRecipe.tickTasks()
-    #             recipes.state.currentRecipe.checkStepCompletion()
-    #         MUTEX.release()
-            
-    #         if HALT.is_set():
-    #             microlabHardware.turnOffEverything()
-    #             break
-
-    # microlab = threading.Thread(target=runMicrolab)
-    # microlab.start()
-
-    # def handleSignal(_a, _b):
-    #     logging.info("")
-    #     logging.info("Shutting down microlab.")
-    #     HALT.set()
-    #     microlab.join()
-    #     logging.info("Shutdown completed.")
-    #     sys.exit()
-
-    # signal.signal(signal.SIGINT, handleSignal)
-    # signal.signal(signal.SIGTERM, handleSignal)
-
-    # def reloadHardware():
-    #     logging.info("Reloading microlab device configuration")
-    #     hardwareConfig = hardware.devicelist.loadHardwareConfiguration()
-    #     deviceDefinitions = hardwareConfig['devices']
-    #     return microlabHardware.loadHardware(deviceDefinitions)
-
-    # commandDict = {
-    #   "start": recipes.core.start,
-    #   "status": recipes.core.status,
-    #   "stop": recipes.core.stop,
-    #   "selectOption": recipes.core.selectOption,
-    #   "reloadConfig": lambda x: config.reloadConfig(),
-    #   "reloadHardware": lambda x: reloadHardware(),
-    # }
-
-    # while True:
-    #     time.sleep(0.01)
-    #     if not in_queue.empty():
-    #         data = in_queue.get() # Receive data
-    #         # status just fetches data and so doesn't need a lock, everything
-    #         # else is a mutation and needs a lock to prevent conflicts with
-    #         # the other thread
-    #         if data["command"] == "status":
-    #             result = commandDict[data["command"]](data["args"])
-    #         else:
-    #             MUTEX.acquire()
-    #             result = commandDict[data["command"]](data["args"])
-    #             MUTEX.release()
-    #         if result is not None:
-    #             out_queue.put(result) # Send data back
